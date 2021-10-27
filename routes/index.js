ed031c4def717f2a2ed5c820b472734dbbb7a955
mw3981 <mw3981@nyu.edu>
2021-10-27 18:58:20 -0400

    Adding script

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Hellow World in Routes");
+  res.send("Hello World in Routes");
 });
 
 module.express = router;
